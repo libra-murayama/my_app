8ce3f8e224d74a2c9045d352f9409b0d517a845a
libra-murayama <neugate.murayama@gmail.com>
2022-09-26 13:48:50 +0900

    2nd

--- my_app/settings.py
+++ my_app/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,9 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-noc1tpl#($xyu#&v=y0s1_w6sn6vtmh^de50o8i%)9^tdsibr!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False#Falseに変える
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS=[os.environ.get("ALLOWED_HOSTS")]
+#ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -122,6 +123,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT="/usr/share/nginx/html/static"
+
+MEDIA_ROOT="/usr/share/nginx/html/media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
